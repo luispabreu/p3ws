72d5590fae0f9016b7ea200df79f965e7ac3b098
lep43 <lep43@duke.edu>
2020-12-12 14:16:34 -0500

    added max.py

--- 04_max/max.py
+++ 04_max/max.py
@@ -1,10 +1,11 @@
 def max(num1, num2):
     # check if num1 is greater than num2
-
+    if (num1 > num2):
         # if so, your answer is num1
-
+        return num1
         # otherwise, your answer is num2
-
+        pass
+    return num2
     pass
         
 def main():
@@ -12,7 +13,7 @@ def main():
     print('max(33, 0) is ' + str(max(33, 0)))
     print('max(0.123, 0.223) is ' + str(max(0.123, 0.223)))
     # print the max of -0.123 and -0.223
-
+    print('max(-0.123, -0.223) is ' + str(max(-0.123,-0.223)))
     return 0
 
 main()
