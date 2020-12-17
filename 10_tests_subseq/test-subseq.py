0fc8f29f33d7d9a6f99a2363b5b9040755caaf22
lep43 <lep43@duke.edu>
2020-12-16 19:37:42 -0500

    added test-subseq.py

--- 10_tests_subseq/test-subseq.py
+++ 10_tests_subseq/test-subseq.py
@@ -5,3 +5,58 @@ import sys
 if (maxSeq([1,2,3]) != 3):
     sys.exit(1)
     pass
+
+if (maxSeq([2,5,100]) != 3):
+    sys.exit(1)
+    pass
+
+if (maxSeq([1,2,3,1,8]) != 3):
+    sys.exit(1)
+    pass
+
+if (maxSeq([1,1,1]) != 1):
+    sys.exit(1)
+    pass
+
+if (maxSeq([]) != 0):
+    sys.exit(1)
+    pass
+
+if (maxSeq([3,2,1]) != 1):
+    sys.exit(1)
+    pass
+
+if (maxSeq([1]) != 1):
+    sys.exit(1)
+    pass
+
+if (maxSeq([-1000,2,3]) != 3):
+    sys.exit(1)
+    pass
+
+if (maxSeq([1,2,1,2,3]) != 3):
+    sys.exit(1)
+    pass
+
+if (maxSeq([-1,-2,-3]) != 1):
+    sys.exit(1)
+    pass
+
+if (maxSeq([-1,0,1]) != 3):
+    sys.exit(1)
+    pass
+
+if (maxSeq([1,2,3,1,2]) != 3):
+    sys.exit(1)
+    pass
+
+if (maxSeq([1,2,1,2,3,1,2]) != 3):
+    sys.exit(1)
+    pass
+
+if (maxSeq([65,97]) != 2):
+    sys.exit(1)
+    pass
+
+
+
