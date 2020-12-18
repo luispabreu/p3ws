b45ae55209b7f947c38be97cb709b82137ab6405
lep43 <lep43@duke.edu>
2020-12-18 00:50:46 -0500

    new formula from Wolfram

--- 13_circle/circle.py
+++ 13_circle/circle.py
@@ -36,12 +36,11 @@ class Circle:
             pass
         else:
             d1 = (self.r**2 - other_circle.r**2 + distance**2) / (2 * distance)
-            d2 = math.sqrt(self.r**2 - d1**2)
-            a = self.r**2 + math.asin(d2 / self.r)
-            b = other_circle.r**2 * math.asin(d2 / other_circle.r)
-            c = d1 * math.sqrt(self.r**2 - d2**2)
-            d = d2 * math.sqrt(other_circle.r**2 - d2**2)
-            return a + b - c - d
+            d2 = distance - d1
+            a = self.r**2 * math.acos((distance**2 + self.r**2 - other_circle.r**2) / (2 * distance * self.r))
+            b = other_circle.r**2 * math.acos((distance**2 + other_circle.r**2 - self.r**2) / (2* distance * other_circle.r))
+            c = .5 * math.sqrt((- distance + self.r + other_circle.r)*(distance + self.r + other_circle.r)*(distance + self.r + other_circle.r))
+            return a + b - c
         pass
     
         
