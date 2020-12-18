0d7bc32d6551aad1f6b0b46baee377e7029cdefc
lep43 <lep43@duke.edu>
2020-12-18 09:26:10 -0500

    added cp.py

---
+++ 14_closest_point/cp.py
@@ -0,0 +1,28 @@
+import math
+
+from point import Point
+
+def closestPoint(s,p):
+    # If S has 0 point in it, give an answer of "no answer exists"
+    assert len(s) > 0
+    first = 1
+    
+    for i in s:
+        # Compute the distance between So & p, call it bestDistance
+        currentDistance = p.distance_from(i)
+
+        if first == 1:
+            # Start with the best choice of So
+            bestChoice = i
+            bestDistance = currentDistance
+            pass
+        else:
+            # If current distance is smaller than bestDistance
+            if currentDistance > bestDistance:
+                # Update bestChoice to Si and bestDistance to currentDistance
+                bestDistance == currentDistance
+                bestChoice == i
+                pass
+            pass
+        # Give anser of bestChoice
+        return bestDistance
